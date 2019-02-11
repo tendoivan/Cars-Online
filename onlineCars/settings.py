f6f68c768a5d5d93cec5c88014f50e8dd7003319
mcnetug <mcnetugandaltd@gmail.com>
2019-02-11 15:33:48 +0700

    onlineCars/settings.py

--- onlineCars/settings.py
+++ onlineCars/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+	'onlineCarsApp',
+	'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +56,7 @@ ROOT_URLCONF = 'onlineCars.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['/Python/Projects/onlineCars/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
